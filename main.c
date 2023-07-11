ec1f2a5b3b88c41865999af60eada24bea7f2562
rajeevtapadia <rtapadia730@gmail.com>
2023-07-11 19:07:28 +0530

    Added comments

--- main.c
+++ main.c
@@ -4,11 +4,14 @@
 
 #include <stdio.h>
 
+// array for input data with zeros appended manually (automation required)
 int input[11] = {1,1,0,0,0,1,1,0,0,0,0};
 int inp_len = 11;
+
+// array for the key
 int key[5] = {1, 1, 0, 0, 1};
 
-// initially temp has same content as input
+// initially temp has same content as first n bits as input
 int temp[5] = {1,1,0,0,0};
 
 const int key_len = 5;
@@ -21,19 +24,20 @@ void printArr(int *arr, int size){
     printf("\n");
 }
 
+// shifts bits till the MSB value is 1
 void shift(){
     do{
+        // shift bits
         for(int i = 0; i< key_len-1; i++){
             temp[i] = temp[i+1];
         }
+        // appending input bit to the temp array
         temp[key_len-1] = input[inp_head_index];
         inp_head_index++;
     }while(temp[0] == 0);
 }
 
 void XOR(){
-//    int temp_g = temp;
-
     for(int i = 0; i < key_len; i++){
         temp[i] = temp[i] ^ key[i];
     }
